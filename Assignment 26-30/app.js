57152452f492eec9119c20ce3d4f7634b2981e4a
Tahirah Roohi <130206321+TahirahRoohi271@users.noreply.github.com>
2023-06-23 12:41:06 +0500

    Add files via upload

---
+++ Assignment 26-30/app.js
@@ -0,0 +1,65 @@
+// Question 1
+
+// var input = parseInt(prompt("Enter any positive integer"));
+// var rounded = Math.round(input);
+// var floor = Math.floor(input);
+// var ceil = Math.ceil(input);
+// document.write("Number: " + input + "<br>" + "Round Off Value: " + rounded + "<br>" + "Floor Value: " + floor + "<br>" + "Ceil Value: " + ceil);
+
+
+// Question 2
+
+// var input = parseFloat(prompt("Enter any negative integer"));
+// var rounded = Math.round(input);
+// var floor = Math.floor(input);
+// var ceil = Math.ceil(input);
+// document.write("Number: " + input + "<br>" + "Round Off Value: " + rounded + "<br>" + "Floor Value: " + floor + "<br>" + "Ceil Value: " + ceil);
+
+
+// Question 3
+
+// var input = parseFloat(prompt("Enter any number"));
+// var absolute = Math.abs(input);
+// document.write("The absolute value of " + input + " is " + absolute);
+
+
+// Question 4
+
+// var dice = Math.floor(Math.random() * 6) + 1;
+// document.write("Random dice value is: " + dice);
+
+
+// Question 5
+
+// var coin = Math.floor(Math.random() * 2) + 1;
+// var result;
+// if (coin === 2) {
+//     result = "Heads";
+// } else {
+//     result = "Tails";
+// }
+// document.write(coin + "<br>" + "Random coin value: " + result);
+
+
+// Queston 6
+
+// var number = Math.floor(Math.random() * 100) + 1;
+// document.write("Random number between 1 and 100: " + number);
+
+
+// Question 7
+
+// var input = prompt("Enter your weight:");
+// var replacement = input.replace(/[^\d.]/g, '');
+// document.write("The weight of the user is: " + replacement + " kilograms");
+
+
+// Question 8
+
+// var number = Math.floor(Math.random() * 10) + 1;
+// var input = parseInt(prompt("Guess the secret number between 1 and 10"));
+// if (input === number) {
+//   alert("Congratulations! You guessed the secret number.");
+// } else {
+//   alert("Try again!" + "\n" + "The Secret number is " + number);
+// }
